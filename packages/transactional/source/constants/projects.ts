7cd9b4e8e32a8f6aaa183565b3597cffb0953761
teteumachado <trintascripter@hotmail.com>
2026-07-14 20:05:36 -0300

    feat: add shortDescription to projects

--- packages/transactional/source/constants/projects.ts
+++ packages/transactional/source/constants/projects.ts
@@ -1,6 +1,7 @@
 export interface Project {
   name: string
   description: string
+  shortDescription: string
   url: string
 }
 
@@ -9,36 +10,7 @@ export const projects: Project[] = [
     name: 'Trueskill Preference Research App',
     description:
       'Sistema de pesquisa de preferências baseado no algoritmo TrueSkill, que gera rankings inteligentes por meio de comparações entre opções.',
+    shortDescription: 'Sistema de pesquisas de prefereência baseado em TrueSkill',
     url: 'https://github.com/teteumachado/trueskill-preference-research',
-  },
-  {
-    name: 'Teteu Machado',
-    description:
-      'Site pessoal com blog, portfólio e experimentos. Construído com Next.js, Tailwind v4 e shadcn/ui.',
-    url: 'https://teteumachado.com',
-  },
-  {
-    name: 'Another Project',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    url: 'https://github.com/teteumachado1',
-  },
-  {
-    name: 'Yet Another Project',
-    description:
-      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    url: 'https://github.com/teteumachado2',
-  },
-  {
-    name: 'Another Project',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    url: 'https://github.com/teteumachado3',
-  },
-  {
-    name: 'Yet Another Project',
-    description:
-      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    url: 'https://github.com/teteumachado4',
   },
 ]

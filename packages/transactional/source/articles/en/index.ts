b0fbd89688c277966d83e8c5de6da098fc5832e0
teteumachado <trintascripter@hotmail.com>
2026-07-17 22:59:41 -0300

    feat: translate trueskill post to english

--- packages/transactional/source/articles/en/index.ts
+++ packages/transactional/source/articles/en/index.ts
@@ -1,1 +1,5 @@
-export const mdxModules = {} as const
+import TrueskillRankingAlgorithmMarketing from './trueskill-ranking-algorithm-marketing.mdx'
+
+export const mdxModules = {
+  'trueskill-ranking-algorithm-marketing': TrueskillRankingAlgorithmMarketing,
+} as const
